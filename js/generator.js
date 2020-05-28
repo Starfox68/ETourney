b34f1290d81d86ec6f4bbb56e44dfbfb18e0a7fd
Parth More <parthmore0116@gmail.com>
2020-05-28 00:12:34 -0400

    Intake Page for Generator

---
+++ js/generator.js
@@ -0,0 +1,6 @@
+function othername() {
+    var input = document.getElementById("tourneyName").value;
+    var input2 = document.getElementById("participants").value;
+    alert(input);
+    alert(input2);
+}
